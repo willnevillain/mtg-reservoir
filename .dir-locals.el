e70e92fd76e5d50c5ec53f5e23a70c637ed23fd2
Will Neville <william@neville.com>
2020-03-07 14:18:12 -0500

    .dir-locals file that loads contents from .env

---
+++ .dir-locals.el
@@ -0,0 +1,15 @@
+((nil .
+      ((eval .
+             (progn
+               (require 'f)
+               (require 's)
+               (let ((maybe-env-path (concat(projectile-project-root) ".env")))
+                 (when (s-present? (concat (projectile-project-root) ".env"))
+                   (let (
+                         (pairs (mapcar #'(lambda (it) (s-split "=" it))
+                                        (seq-filter #'s-present? (s-lines (f-read (concat (projectile-project-root) ".env"))))))
+                         )
+                     (dolist (pair pairs)
+                       (setenv
+                        (car pair)
+                        (car (cdr pair))))))))))))
