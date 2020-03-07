870ce444d9f00896b090ad9afd1fbdfd6f02dca9
Will Neville <william@neville.com>
2020-03-07 12:51:13 -0500

    Indium config fix, define baby routes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
+const express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mtg', { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv/config');
 
+const app = express();
+
+console.log('Hello, World!');
+// Routes
+app.get('/', (req, res) => {
+  res.send('Root path');
+});
+
+
+// DB Connection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-
-  // Schema methods - note to not use arrow function as they treat 'this' differently
-  catSchema.methods.meow = function () {
-    console.log('Feed me bitch or else you will forever rue the day you crossed ' + this.name);
-  };
-
-  // Compile model and make an instance
-  const Cat = mongoose.model('Cat', catSchema);
-  
-  const winslow = new Cat({ name: 'Winslow', color: 'Grey and Brown' });
-  winslow.save((err, winslow) => {
-    if (err) return console.error(err);
-    winslow.meow();
-  });
-
-  const bundles = new Cat({ name: 'Bundles', color: 'Black' });
-  bundles.save((err, bundles) => {
-    if (err) return console.error(err);
-    bundles.meow();
-  });
-
-  Cat.find((err, cats) => {
-    if (err) return console.error(err);
-    console.log(cats);
-  });
-
-  Cat.find({ name: /^Winslow/ }, (err, cats) => {
-    if (err) return console.error(err);
-    console.log(cats);
-  });
+  console.log('Connection to DB established.');
 });
+
+
+// Begin listening
+app.listen(3000);
