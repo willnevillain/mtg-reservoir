923a474ed2b82c0995e73f78e549790768c4af88
Will Neville <william@neville.com>
2020-03-08 15:36:31 -0400

    printings -> printing for route as well

--- src/routes/printing.js
+++ src/routes/printing.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
+const Printing = mongoose.model('Printing');
 
 router.get('/', (req, res) => {
   res.send('This will be a list of card printings, with optional query param filters');
