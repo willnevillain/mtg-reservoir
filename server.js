04cf288d90e31e195950012f3ae48f9181ff647a
Will Neville <william@neville.com>
2020-03-09 12:38:16 -0400

    Cleaning up server.js

--- server.js
+++ server.js
@@ -1,34 +1,51 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.json());
 
-const printingsRoute = require('./routes/printings');
-
-
-app.use('/printings', printingsRoute);
-
-// Routes
+// Start Route Definitions
 app.get('/', (req, res) => {
   res.send('Root path');
 });
+// End Route Definitions
 
+// Start DB Connection and server start logic
+const connect = () => {
+  mongoose.connect(
+    process.env.DB_CONNECTION,
+    {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err) => {
+      const dbStatus = err ? `* DB Connection - Error: ${err}` : `* DB Connection - OK`;
+      console.log('********************');
+      console.log('* Server Starting');
+      console.log(dbStatus);
+    }
+  );
+};
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+// End DB Connection Logic
 
-// DB Connection
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connection to DB established.');
-});
-
+// Start middleware registration
 app.use((err, req, res, next) => {
   return res.status(500).json({ error: err });
 });
+// End middleware registration
 
+// Start listening for requests
+app.listen(app.get('port'), (err) => {
+  console.log(`* Port: ${app.get('port')}`);
+  console.log(`* Started listening at: ${new Date().toUTCString()}`);
+  console.log('********************');
+});
 
-// Begin listening
-app.listen(3000);
+module.exports = app;
