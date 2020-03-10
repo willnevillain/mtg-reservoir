3eb7ebd2438e37fc6f0631773232365644a3bc01
Will Neville <william@neville.com>
2020-03-10 15:00:51 -0400

    Optimize import script

--- scripts/mtg-importer.js
+++ scripts/mtg-importer.js
@@ -7,13 +7,11 @@ const filePath = path.resolve(__dirname, '../data/AllPrintings.json');
 
 module.exports = () => {
   const contents = JSON.parse(fs.readFileSync(filePath));
-  Object.keys(contents).map(setId => contents[setId]).forEach(set => {
+  Object.keys(contents).forEach((setId) => {
+    const set = contents[setId];
     Set.create({ ...set });
-    set.cards.forEach(card => {
-      card.printing = set.code;
-      Printing.create({ ...card });
+    set.cards.forEach((card) => {
+      Printing.create({ ...card, printing: set.code });
     });
   });
 };
-
-
